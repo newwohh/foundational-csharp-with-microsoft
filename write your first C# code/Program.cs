fa27bd7aebba73c074675cdcfb35c6f5247a3463
newwohh <nevoznvx@gmail.com>
2023-08-29 17:48:45 +0530

    first-program

--- write your first C# code/Program.cs
+++ write your first C# code/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// 1
 Console.WriteLine("Hello, World!");
+// 3
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
+// 5
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
