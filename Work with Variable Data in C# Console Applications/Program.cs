fbb7a11dbb91e86e6454ed9d6cc26f819b46fffc
newwohh <nevoznvx@gmail.com>
2023-09-15 18:00:31 +0530

    variable-and-data-types

--- Work with Variable Data in C# Console Applications/Program.cs
+++ Work with Variable Data in C# Console Applications/Program.cs
@@ -70,3 +70,311 @@ Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {r
 
 float result3 = value3 / value1;
 Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+string[] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+
+string val = "abc123";
+char[] valueArray = val.ToCharArray();
+Array.Reverse(valueArray);
+
+string res = String.Join(",", valueArray);
+Console.WriteLine(res);
+
+string[] items = res.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+string[] message = pangram.Split(' ');
+
+string[] newMessage = new string[message.Length];
+
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+}
+
+string results = String.Join(" ", newMessage);
+Console.WriteLine(results);
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items1 = orderStream.Split(',');
+Array.Sort(items1);
+
+foreach (var item in items1)
+{
+    if (item.Length == 4)
+    {
+        Console.WriteLine(item);
+    }
+    else
+    {
+        Console.WriteLine(item + "\t- Error");
+    }
+}
+
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+Console.WriteLine(yourDiscount);
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
+
+string message2 = "Find what is (inside the parentheses)";
+
+int openingPosition1 = message2.IndexOf('(');
+int closingPosition2 = message2.IndexOf(')');
+
+Console.WriteLine(openingPosition1);
+Console.WriteLine(closingPosition2);
+
+int length = closingPosition2 - openingPosition1;
+Console.WriteLine(message2.Substring(openingPosition1, length));
+
+string message1 = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+char[] openSymbols = { '[', '{', '(' };
+
+int closingPosition1 = 0;
+
+// while (true)
+// {
+//     int openingPosition12 = message2.IndexOfAny(openSymbols, closingPosition1);
+
+//     if (openingPosition12 == -1) break;
+
+//     string currentSymbol = message1.Substring(openingPosition12, 1);
+
+//     char matchingSymbol = ' ';
+
+//     switch (currentSymbol)
+//     {
+//         case "[":
+//             matchingSymbol = ']';
+//             break;
+//         case "{":
+//             matchingSymbol = '}';
+//             break;
+//         case "(":
+//             matchingSymbol = ')';
+//             break;
+//     }
+
+
+//     openingPosition12 += 1;
+//     closingPosition1 = message.IndexOf(matchingSymbol, openingPosition12);
+
+//     int length = closingPosition1 - openingPosition12;
+//     Console.WriteLine(message.Substring(openingPosition12, length));
+// }
+
+string data12 = "12345John Smith          5000  3  ";
+string updatedData12 = data12.Remove(5, 20);
+Console.WriteLine(updatedData12);
+
+string message123 = "This--is--ex-amp-le--da-ta";
+message123 = message123.Replace("--", " ");
+message123 = message123.Replace("-", "");
+Console.WriteLine(message123);
+
+string animalSpecies = "";
+string animalID = "";
+string animalAge = "";
+string animalPhysicalDescription = "";
+string animalPersonalityDescription = "";
+string animalNickname = "";
+
+int maxPets = 8;
+string? readResult;
+string menuSelection = "";
+
+string[,] ourAnimals = new string[maxPets, 6];
+
+// #4 create sample data ourAnimals array entries
+for (int i = 0; i < maxPets; i++)
+{
+    switch (i)
+    {
+        case 0:
+            animalSpecies = "dog";
+            animalID = "d1";
+            animalAge = "2";
+            animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 45 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+            animalNickname = "lola";
+            break;
+
+        case 1:
+            animalSpecies = "dog";
+            animalID = "d2";
+            animalAge = "9";
+            animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+            animalNickname = "gus";
+            break;
+
+        // case 2: deleted for brevity
+        // case 3: deleted for brevity
+
+        default:
+            animalSpecies = "";
+            animalID = "";
+            animalAge = "";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+            break;
+    }
+    ourAnimals[i, 0] = "ID #: " + animalID;
+    ourAnimals[i, 1] = "Species: " + animalSpecies;
+    ourAnimals[i, 2] = "Age: " + animalAge;
+    ourAnimals[i, 3] = "Nickname: " + animalNickname;
+    ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
+    ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
+}
+
+// do
+// {
+//     // NOTE: the Console.Clear method is throwing an exception in debug sessions
+//     Console.Clear();
+
+//     Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+//     Console.WriteLine(" 1. List all of our current pet information");
+//     Console.WriteLine(" 2. Display all dogs with a specified characteristic");
+//     Console.WriteLine();
+//     Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+
+//     readResult = Console.ReadLine();
+//     if (readResult != null)
+//     {
+//         menuSelection = readResult.ToLower();
+//     }
+
+//     // use switch-case to process the selected menu option
+//     switch (menuSelection)
+//     {
+//         case "1":
+//             // list all pet info
+//             for (int i = 0; i < maxPets; i++)
+//             {
+//                 if (ourAnimals[i, 0] != "ID #: ")
+//                 {
+//                     Console.WriteLine();
+//                     for (int j = 0; j < 6; j++)
+//                     {
+//                         Console.WriteLine(ourAnimals[i, j]);
+//                     }
+//                 }
+//             }
+//             Console.WriteLine("\n\rPress the Enter key to continue");
+//             readResult = Console.ReadLine();
+//             break;
+
+//         case "2":
+//             // Display all dogs with a specified characteristic");
+//             Console.WriteLine("\nUNDER CONSTRUCTION - please check back next month to see progress.");
+//             Console.WriteLine("Press the Enter key to continue.");
+//             readResult = Console.ReadLine();
+//             break;
+
+//         default:
+//             break;
+//     }
+
+// } while (menuSelection != "exit");
